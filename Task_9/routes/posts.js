079b399fcf34d653a44100fd57b60de749ee6375
Rachna-11 <jariyarachna11@gmail.com>
2025-06-30 15:05:34 +0530

    Initial commit

---
+++ Task_9/routes/posts.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// Create a blog post
+router.post('/', async (req, res) => {
+  try {
+    const post = new Post(req.body);
+    const savedPost = await post.save();
+    res.status(201).json(savedPost);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Get all blog posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 });
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
