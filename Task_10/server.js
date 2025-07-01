4ec913f153f82bdc85cdb4e420bceb91aa38c096
Rachna-11 <jariyarachna11@gmail.com>
2025-07-01 23:09:44 +0530

    Initial commit

--- Task_10/server.js
+++ Task_10/server.js
@@ -13,8 +13,12 @@ app.use('/api/auth', authRoutes);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.error('MongoDB connection failed:', err.message);
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
